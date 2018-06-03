d45118ba5f84bae78330901462a51d23c72f24a2
HalgoBZZ <hafedh.baazouzi@gmail.com>
2018-06-03 08:08:23 +0200

    map is ok help is ok

---
+++ app/src/main/java/com/halgo/myapplication/DetailsTourneeActivity.java
@@ -0,0 +1,12 @@
+package com.halgo.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailsTourneeActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_tournee);
+    }
+}
